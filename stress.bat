bc48036717f9a5de22672b45f55178cc607f6f3c
Michael Ryan <torican@gmail.com>
2015-10-07 14:46:30 +1300

    Update Timeout

--- stress.bat
+++ stress.bat
@@ -4,7 +4,7 @@ SET /a i=0
 IF %i%==60 GOTO END
 echo This is cycle number %i%.
 Start "c:\Program Files (x86)\Notepad++\" notepad++.exe
-TIMEOUT /T 2
+TIMEOUT /T 120
 taskkill /F /IM notepad++.exe
 SET /a i=%i%+1
 GOTO LOOP
